18947e0e2fd1c1df3c23c6766e908be0092c51f4
nikhilraj081 <Nikhil.Raj3@cerner.com>
2024-03-19 15:15:38 +0530

    updated product controller

--- shopping-cart-service/src/main/java/com/shoppingcart/rest/shoppingcartservice/Controller/ProductController.java
+++ shopping-cart-service/src/main/java/com/shoppingcart/rest/shoppingcartservice/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.shoppingcart.rest.shoppingcartservice.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shoppingcart.rest.shoppingcartservice.Exceptions.ResourceNotFoundException;
 import com.shoppingcart.rest.shoppingcartservice.Model.Product;
 import com.shoppingcart.rest.shoppingcartservice.Services.ProductService;
@@ -22,6 +28,12 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
+    
+    @Value("${image.path}")
+    private String path;
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @GetMapping("/all")
     public ResponseEntity<?> getAllProduct() throws ResourceNotFoundException
@@ -52,12 +64,33 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(product); 
     }
 
-    @PostMapping
-    public ResponseEntity<?> seProduct(@RequestBody Product product)
+    @PostMapping("/seller/{sellerId}")
+    public ResponseEntity<?> seProduct(@RequestParam("image") MultipartFile[] image, @RequestParam("productData") String productData)
     {
-        Product productDetails = productService.setProduct(product);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productDetails);
-    }
+        //convert string to json
+        Product product = null;
+        try {
+            product = mapper.readValue(productData, Product.class);
+        } catch (JsonMappingException e) {
+           
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+           
+            e.printStackTrace();
+        }
 
+        // Product newProduct = productService.setProduct(product);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+
+        for (MultipartFile multipartFile : image) {
+            System.out.println(multipartFile.getOriginalFilename());
+            
+        }
+
+        System.out.println(product.getProductName());
+
+
+        return ResponseEntity.status(HttpStatus.OK).body("done");
+    }
 
 }
